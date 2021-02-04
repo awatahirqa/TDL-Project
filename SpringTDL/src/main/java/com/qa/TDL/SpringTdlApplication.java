49e8018326347eec449c626156c410770e35ee3c
Waleed <WTahir@qa.com>
2021-02-04 15:16:32 +0000

    Project initialisation

---
+++ SpringTDL/src/main/java/com/qa/TDL/SpringTdlApplication.java
@@ -0,0 +1,14 @@
+package com.qa.TDL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class SpringTdlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTdlApplication.class, args);
+	}
+
+}
---
+++ SpringTDL/src/test/java/com/qa/SpringTDL/SpringTdlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.SpringTDL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTdlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
