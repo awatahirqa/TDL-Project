65a8b60e65ec0cd2496c14bab54f10eb59f7d087
Waleed <WTahir@qa.com>
2021-02-09 15:22:04 +0000

    Finished testing for Domain and DTO and adjusted mistake in DTO class

--- SpringTDL/src/main/java/com/qa/TDL/persistance/dto/ToDoListDTO.java
+++ SpringTDL/src/main/java/com/qa/TDL/persistance/dto/ToDoListDTO.java
@@ -8,31 +8,46 @@ public class ToDoListDTO {
 	
 	
 	private Long list_id;
+	private String name;
 	private List<TasksDomain> tasklist;
 	public ToDoListDTO() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-	public ToDoListDTO(Long list_id, List<TasksDomain> tasklist) {
+	
+	public ToDoListDTO(Long list_id, String name, List<TasksDomain> tasklist) {
 		super();
 		this.list_id = list_id;
+		this.name = name;
 		this.tasklist = tasklist;
 	}
+
 	public Long getList_id() {
 		return list_id;
 	}
 	public void setList_id(Long list_id) {
 		this.list_id = list_id;
 	}
-	public List<TasksDomain> getCatlist() {
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public List<TasksDomain> gettasklist() {
 		return tasklist;
 	}
 	public void setTasklist(List<TasksDomain> tasklist) {
 		this.tasklist = tasklist;
 	}
+
 	@Override
 	public String toString() {
-		return "ToDoListDomain [list_id=" + list_id + ", tasklist=" + tasklist + "]";
+		return "ToDoListDTO [list_id=" + list_id + ", name=" + name + ", tasklist=" + tasklist + "]";
 	}
+	
 	
 }
